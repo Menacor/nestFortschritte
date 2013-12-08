ff3129fbc8591483ee18e6cf7f73dd6232d3d994
Patrick Tekath <patrick.tekath@rub.de>
2013-12-08 03:27:01 -0800

    Added raw_input for files

--- nestStudienprojekt.py
+++ nestStudienprojekt.py
@@ -1,20 +1,23 @@
 import nest
-import numpy
 import nest.topology as tp
+import csv
 
-#Creating different layers in nest for training purposes
-testLayer = tp.CreateLayer({'rows' : 5, 'columns' : 5, 'elements' : 'iaf_neuron'})
-layerCA1 = tp.CreateLayer({'rows' : 7, 'columns' : 3, 'extent' : [2.0,0.5], 'elements' : 'iaf_neuron'})
-layerCA2 = tp.CreateLayer({'rows' : 4, 'columns' : 4, 'elements' : 'iaf_neuron', 'center' : [0.5,0.5]})
-layerCA3 = tp.CreateLayer({'rows' : 3, 'columns' : 4, 'extent' : [1.3, 0.7], 'elements' : 'iaf_neuron', 'center' : [1.0,1.0]})
+#initiallizing list elements for neuron sets and connections
+neuronSets = []
+connections = []
 
-#Creating free layers
-pos = [[numpy.random.uniform(-0.5,0.5), numpy.random.uniform(-0.5,0.5)] 
-for j in xrange (50)]
+inputSets = raw_input("Specify a file name for the neuronSets: ")
+inputConnections = raw_input("Specify a file name for the connections: ")
 
-layerFreePos = tp.CreateLayer({'positions' : pos, 'elements' : 'iaf_neuron'})
+neuronReader = csv.reader(open(inputSets, "rb")) 
+connectionsReader = csv.reader(open(inputConnections, "rb")) 
 
-#Creating a 3d layer
-pos = [[numpy.random.uniform(-0.5,0.5), numpy.random.uniform(-0.5,0.5), numpy.random.uniform(-0.5,0.5)] for k in xrange (200)]
+for row in neuronReader:
+ neuronSets.append(row)
 
+for row in connectionsReader:
+ connections.append(row)
 
+print neuronSets
+print
+print connections
